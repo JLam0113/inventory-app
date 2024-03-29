a69bd56d7df4eacccacf0ad0c6cb737d918d2fe7
Jessye Lam <jessyel13@gmail.com>
2024-03-29 15:45:20 -0400

    added update form

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,14 +1,29 @@
 const Item = require("../models/item");
-const ObjectId = require('mongoose').Types.ObjectId; 
-
+const ObjectId = require('mongoose').Types.ObjectId;
+const Category = require("../models/category");
 
 const asyncHandler = require("express-async-handler");
 
-exports.display = asyncHandler(async (req, res, next) => {
-    const allItems = await Item.find({ "category" : new ObjectId(req.params.id)}).exec();
-    console.log(allItems);
-    res.render("item_list", {
-      title: "Items",
-      items: allItems,
-    });
+exports.index = asyncHandler(async (req, res, next) => {
+  const allItems = await Item.find({ "category": new ObjectId(req.params.id) }).exec();
+  console.log(allItems);
+  res.render("item_list", {
+    title: "Items",
+    items: allItems,
   });
+});
+
+exports.display = asyncHandler(async (req, res, next) => {
+  const itemQuery = await Item.find({ "_id": new ObjectId(req.params.id) }).exec();
+  const allCategories = await Category.find().sort({ name: 1 }).exec();
+  res.render("item", {
+    title: itemQuery[0].name,
+    item: itemQuery[0],
+    categories: allCategories,
+  });
+});
+
+exports.update = asyncHandler(async (req, res, next) => {
+  console.log(req);
+  res.redirect('back');
+});
--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,13 @@ const item_controller = require("../controllers/itemController");
 router.get("/", category_controller.index);
 
 // Get all items in the category
-router.get("/category/:id", item_controller.display);
+router.get("/category/:id", item_controller.index);
+
+// Get item details
+router.get("/item/:id", item_controller.display);
+
+// Update item
+router.post("/item/:id/update", item_controller.update);
 
 
 module.exports = router;
