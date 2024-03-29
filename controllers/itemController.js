6b350e7f79c1a5bd9ed5369f3700b42d5fcddaff
Jessye Lam <jessyel13@gmail.com>
2024-03-29 16:08:21 -0400

    added update item to item controller

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -2,6 +2,7 @@ const Item = require("../models/item");
 const ObjectId = require('mongoose').Types.ObjectId;
 const Category = require("../models/category");
 
+const { body, validationResult } = require("express-validator");
 const asyncHandler = require("express-async-handler");
 
 exports.index = asyncHandler(async (req, res, next) => {
@@ -23,7 +24,33 @@ exports.display = asyncHandler(async (req, res, next) => {
   });
 });
 
-exports.update = asyncHandler(async (req, res, next) => {
-  console.log(req);
-  res.redirect('back');
-});
+exports.update = [
+  body("name", "Name must be specified").trim().isLength({ min: 1 }).escape(),
+  body("description", "description must be specified").trim().isLength({ min: 1 }).escape(),
+  body("category", "category must be specified").trim().isLength({ min: 1 }).escape(),
+  body("price", "price must be specified").trim().optional({ checkFalsy: true }).isNumeric().withMessage('Only Decimals allowed'),
+  body("quantity", "quantity must be specified").trim().isInt({ min: 0 }),
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+    const item = new Item({
+      name: req.body.name,
+      description: req.body.description,
+      category: new ObjectId(req.body.category),
+      price: parseFloat(req.body.price),
+      quantity: parseInt(req.body.quantity),
+      _id: req.params.id,
+    });
+    console.log(req.params.id);
+    console.log(item);
+
+    if (!errors.isEmpty()) {
+      // There are errors.
+      // Render the form again, passing sanitized values and errors.
+      console.log("error")
+      res.redirect('back');
+      return;
+    }
+    else {
+      await Item.findByIdAndUpdate(req.params.id, item, {});
+    }
+    }),];
